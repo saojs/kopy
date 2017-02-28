91887decee1dddd4a03c22a241b540f976591d76
EGOIST <0x142857@gmail.com>
2017-02-28 23:33:02 +0800

    prevent from ReferenceErrors

--- src/ask.js
+++ src/ask.js
@@ -6,6 +6,14 @@ export default function ask(data, prompts) {
       inquirer.prompt(prompts)
         .then(answers => {
           const merged = Object.assign({}, data, answers)
+
+          // prevent from ReferenceErrors
+          for (const prompt of prompts) {
+            if (!Object.prototype.hasOwnProperty.call(merged, prompt.name)) {
+              merged[prompt.name] = undefined
+            }
+          }
+
           metalsmith.metadata({data, answers, merged})
           done()
         })
