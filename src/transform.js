e8c2cdefff383b90154272f2f2159d0d14697405
EGOIST <0x142857@gmail.com>
2018-01-06 20:46:13 +0800

    fix lint

--- src/transform.js
+++ src/transform.js
@@ -2,11 +2,15 @@ import match from 'multimatch'
 
 export default transforms => stream => {
   const globs = Object.keys(transforms)
-  return Promise.all(globs.map(glob => {
-    const handler = transforms[glob]
-    const filepaths = match(stream.fileList, glob)
-    return Promise.all(filepaths.map(filepath => {
-      return handler(filepath, stream)
-    }))
-  }))
+  return Promise.all(
+    globs.map(glob => {
+      const handler = transforms[glob]
+      const filepaths = match(stream.fileList, glob)
+      return Promise.all(
+        filepaths.map(filepath => {
+          return handler(filepath, stream)
+        })
+      )
+    })
+  )
 }
