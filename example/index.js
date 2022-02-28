c30dbb7cae5212af99b3bc130dc85de378287569
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-01 00:10:01 +0800

    Delete example directory

--- example/index.js
+++
@@ -1,6 +0,0 @@
-const path = require('path')
-const kopy = require('../lib')
-
-const generator = kopy(require('./saofile'))
-
-generator.run({ outDir: path.join(__dirname, 'dist') }).catch(kopy.handleError)
